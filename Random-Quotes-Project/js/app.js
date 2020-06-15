c6018dda3f088a4c1480bb7c0c046bb50b768c3a
Mohammad Raska <iban.ninja@gmail.com>
2020-06-16 00:15:47 +0700

    project 3, done

--- Random-Quotes-Project/js/app.js
+++ Random-Quotes-Project/js/app.js
@@ -1,17 +1,13 @@
-const quotes = [
-  {
-    quote:
-      "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
+const quotes = [{
+    quote: "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
     author: " Life"
   },
   {
-    quote:
-      "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
+    quote: "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
     author: "John Kenneth Galbraith"
   },
   {
-    quote:
-      "God save me from my friends. I can protect myself from my enemies.",
+    quote: "God save me from my friends. I can protect myself from my enemies.",
     author: "Claude Louis Hector de Villars "
   },
   {
@@ -19,14 +15,24 @@ const quotes = [
     author: "David Thoreau"
   },
   {
-    quote:
-      "Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance. ",
+    quote: "Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance. ",
     author: "Charles Lindbergh"
   },
   {
-    quote:
-      "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
+    quote: "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
     author: " Tyne Daly"
   }
 ];
 
+const button = document.querySelector('button')
+const quote = document.querySelector('#quote')
+const author = document.querySelector('.quote-author')
+
+button.addEventListener('click', changeQuotes)
+
+function changeQuotes() {
+  var quotesSize = Object.keys(quotes).length
+  let valueRandom = Math.floor(Math.random() * quotesSize)
+  quote.textContent = quotes[valueRandom].quote
+  author.textContent = quotes[valueRandom].author
+}
